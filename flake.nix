9b1f6a89f77aba4bfe978701d63109290f8014d4
Yusuke Shibata <yusukeshib@gmail.com>
2026-06-18 22:13:52 -0400

    release: bump version to 0.5.1

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         # (pi/claude) is the user's to provide.
         looop = pkgs.rustPlatform.buildRustPackage {
           pname = "looop";
-          version = "0.5.0";
+          version = "0.5.1";
           src = ./.;
 
           cargoLock.lockFile = ./Cargo.lock;
