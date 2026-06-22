e7acc30f8a824de669b268d7e1e0135dcde10ba8
Yusuke Shibata <yusukeshib@gmail.com>
2026-06-22 07:47:25 -0400

    chore: bump version to 0.16.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         # configured LLM runner (pi/claude) is the user's to provide.
         looop = pkgs.rustPlatform.buildRustPackage {
           pname = "looop";
-          version = "0.15.3";
+          version = "0.16.0";
           src = ./.;
 
           cargoLock.lockFile = ./Cargo.lock;
